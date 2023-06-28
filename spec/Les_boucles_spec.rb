da1a7832552071bb56aee581a2baf9e3639866e1
life light <lifelightdev@gmail.com>
2023-06-28 16:21:51 +0200

    Merge

--- spec/Les_boucles_spec.rb
+++ spec/Les_boucles_spec.rb
@@ -8,7 +8,11 @@ class Les_boucles
   # Ecrire le code qui permet de retourner un tableau en le remplissant progressivement avec une boucle 'for' depuis items
   def  premier_exercice
     items = [1, 2, 3, 4]
-    []
+    # Ecrire ici le code qui permet de retourner un tableau en le remplissant progressivement dans une boucle 'for'
+    retour = []
+    for i in items do
+      retour << i * 3
+    end
   end
 
   RSpec.describe 'Les_boucles' do
@@ -23,7 +27,17 @@ class Les_boucles
   # Ecrire le code qui permet de retourner un tableau contenant seulement les éléments pairs d'items
   def deuxieme_exercice
     items = [6, 9, 2, 1, 4, 0, 18, 3]
-    []
+    # Ecrire ici le code qui permet de retourner un tableau contenant seulement les éléments pairs d'items
+    # Première solution
+    items.map { |element| element if (element % 2) == 0 }.compact
+    # Deuxième solution
+    items.filter_map { |element| element if (element % 2) == 0 }
+    # Troisième solution
+    items.filter { |element| (element % 2) == 0 }
+    # Quatrième solution
+    items.filter { |element| element.even? }
+    # Cinquième solution
+    items.filter(&:even?)
   end
 
   RSpec.describe 'Les_boucles' do
